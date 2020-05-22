935da558a7baecf2c6c1e88d9405fce36e961406
Juan Camilo Flórez <ECM6663K@claro.com.co>
2020-05-21 22:57:30 -0500

    first page results

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 gem 'nokogiri'
 gem 'httparty'
+gem 'byebug'
--- web_scrapper.rb
+++ web_scrapper.rb
@@ -1,9 +1,21 @@
 require 'nokogiri'
 require 'httparty'
+require 'byebug'
 
 def crawl(location)
+    scrapped_data = []
     url = 'https://' + location + '.craigslist.org/search/apa?min_bedrooms=3&max_bedrooms=3&min_bathrooms=2&max_bathrooms=2'
     raw_page = HTTParty.get(url)
     dom_page = Nokogiri::HTML(raw_page)
-    byebug
+    data_block = dom_page.css("//li[@class='result-row']")
+    data_block.each do |rental_home|
+        item = {}
+        item[:title] = rental_home.css('p a.result-title').text
+        item[:address] = rental_home.css('p span.result-hood').text.split.join.delete('(\(|\))')
+        item[:mont_rent] = rental_home.css('p span.result-price').text.delete('^0-9').to_i
+        item[:url] = rental_home.css('a.result-image')[0]['href']
+        
+        puts item
+        scrapped_data.push(item)
+    end
 end
