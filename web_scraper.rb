f932b785e18e7af3757f6d1da2e64e61a3ac6600
Juan Camilo Flórez <ECM6663K@claro.com.co>
2020-05-25 16:43:55 -0500

    merge files

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,6 +1,22 @@
+require 'byebug'
 require 'nokogiri'
 require 'httparty'
-require 'byebug'
+require_relative 'web_scrapper.rb'
+
+def main
+    begin
+        location = ARGV[0]
+        data = crawl(location)
+
+        File.open("data_scraped.csv", "w") {  |f| f.write("title, address, month_rent, url\n") }
+        data.each do |row|
+            File.open("data_scraped.csv", "a+") {  |f| f.write(row + "\n") }
+        end
+    rescue
+        puts "'" + location + "' is not a valid location!"
+    end
+
+end
 
 def crawl(location)
     scrapped_data = []
@@ -29,3 +45,5 @@ def crawl(location)
     end
     return scrapped_data
 end
+
+main
