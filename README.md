0b14edc45b784a240efad24b1bed1e99ab55d57a
Juan Camilo Flórez <ECM6663K@claro.com.co>
2020-05-25 15:53:37 -0500

    exception handling, cli args

---
+++ README.md
@@ -0,0 +1,7 @@
+# A RUBY WEB SCRAPER
+## Dendencies
+- Nokogiri
+- HTTParty
+
+## Usage
+.\main.rb <location_name>
--- main.rb
+++ main.rb
@@ -2,12 +2,16 @@ require 'byebug'
 require_relative 'web_scrapper.rb'
 
 def main
-    location = 'boone'
-    data = crawl(location)
+    begin
+        location = ARGV[0]
+        data = crawl(location)
 
-    File.open("data_scraped.csv", "w") {  |f| f.write("title, address, month_rent, url\n") }
-    data.each do |row|
-        File.open("data_scraped.csv", "a+") {  |f| f.write(row + "\n") }
+        File.open("data_scraped.csv", "w") {  |f| f.write("title, address, month_rent, url\n") }
+        data.each do |row|
+            File.open("data_scraped.csv", "a+") {  |f| f.write(row + "\n") }
+        end
+    rescue
+        puts "'" + location + "' is not a valid location!"
     end
 
 end
